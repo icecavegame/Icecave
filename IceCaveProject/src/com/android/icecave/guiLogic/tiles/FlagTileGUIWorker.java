1cdcef42becf61bf62dfc758c70ccae07a0110ab
icecavegame <sagielord@gmail.com>
2013-09-21 03:43:46 +0300

    implemented the GUITileFactory

---
+++ IceCaveProject/src/com/android/icecave/guiLogic/tiles/FlagTileGUIWorker.java
@@ -0,0 +1,16 @@
+package com.android.icecave.guiLogic.tiles;
+
+import com.android.icecave.guiLogic.TileImageView;
+import com.android.icecave.mapLogic.tiles.ITile;
+
+public class FlagTileGUIWorker implements IGUITileWorker
+{
+
+	@Override
+	public TileImageView makeTile(ITile tile)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- IceCaveProject/src/com/android/icecave/guiLogic/tiles/GUITileFactory.java
+++ IceCaveProject/src/com/android/icecave/guiLogic/tiles/GUITileFactory.java
@@ -1,18 +1,54 @@
 package com.android.icecave.guiLogic.tiles;
 
+import com.android.icecave.mapLogic.tiles.BoulderTile;
+import com.android.icecave.mapLogic.tiles.EmptyTile;
+import com.android.icecave.mapLogic.tiles.FlagTile;
 import com.android.icecave.mapLogic.tiles.ITile;
+import com.android.icecave.mapLogic.tiles.WallTile;
 
 import com.android.icecave.guiLogic.TileImageView;
 
 import java.lang.reflect.Type;
 
 import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.HashMap;
 
+/**
+ * Class holding all the GUITile workers.
+ * @author Tom
+ *
+ */
 public class GUITileFactory
 {
-	private Dictionary<Type, IGUITileWorker> mTileWorkers;
+	/**
+	 * Hashmap for all the workers.
+	 */
+	private HashMap<Class<?>, IGUITileWorker> mTileWorkers;
 	
+	/**
+	 * Create a new instance of the GUITileFactory.
+	 */
+	public GUITileFactory(){
+		mTileWorkers = new HashMap<Class<?>, IGUITileWorker>();
+		
+		mTileWorkers.put(BoulderTile.class, new BoulderTileGUIWorker());
+		mTileWorkers.put(EmptyTile.class, new EmptyTileGUIWorker());
+		mTileWorkers.put(WallTile.class, new WallTileGUIWorker());
+		mTileWorkers.put(FlagTile.class, new FlagTileGUIWorker());
+	}
+	
+	/**
+	 * Get image view for a tile.
+	 * @param tile - To get image of.
+	 * @return Image for a tile.
+	 */
 	public TileImageView getTiles(ITile tile) {
-		return null;
+		// Check if exists.
+		if(!mTileWorkers.containsKey(tile.getClass())){
+			return null;
+		}
+		
+		return mTileWorkers.get(tile.getClass()).makeTile(tile);
 	}
 }
