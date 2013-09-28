187d8eeef2c1fa61c28429f0fa80553003982a65
Sagie Levy <sagielevy21@gmail.com>
2013-09-28 17:28:15 +0300

    Finished move logic...

--- IceCaveProject/src/com/android/icecave/general/Consts.java
+++ IceCaveProject/src/com/android/icecave/general/Consts.java
@@ -17,16 +17,39 @@ public class Consts
 	public final static int DEFAULT_PLAYER = R.drawable.default_player;
 	public final static int DEFAULT_TILES = R.drawable.default_tiles;
 	public final static Point DEFAULT_START_POS = new Point(1, 1);
+	
+	/***
+	 * Level default values
+	 */
+	// Easy
 	public final static int DEFAULT_WALL_WIDTH = 1;
 	public final static int DEFAULT_BOULDER_NUM = 30;
 	public final static int DEFAULT_BOARD_SIZE_X = 10;
 	public final static int DEFAULT_BOARD_SIZE_Y = 10;
-	public final static int DEFAULT_TILES_BMP_COLUMNS = 6;
-	public final static int DEFAULT_TILES_BMP_ROWS = 3;
-	public static final int DEFAULT_PLAYER_BMP_ROWS = 4;
-	public static final int DEFAULT_PLAYER_BMP_COLUMNS = 3;
+	
+	/***
+	 *  Standard tile values and positioning
+	 */
+	// Player tiles
+	public final static int PLAYER_ERROR_ROW = -1;
+	public final static int PLAYER_DOWN_ROW = 0;
+	public final static int PLAYER_LEFT_ROW = 1;
+	public final static int PLAYER_RIGHT_ROW = 2;
+	public final static int PLAYER_UP_ROW = 3;
+	public final static int PLAYER_MOVING_1 = 0;
+	public final static int PLAYER_STANDING = 1;
+	public final static int PLAYER_MOVING_2 = 2;
+	public final static int PLAYER_MOVEMENTS_SUM = 3; // TODO Might want to change these into an enum
+	
+	// Board tiles
 	public final static Point EMPTY_TILE_IN_SPRITE = new Point(4, 0);
 	public final static Point FLAG_TILE_IN_SPRITE = new Point(3, 0);
 	public final static Point WALL_TILE_IN_SPRITE = new Point(4, 2);
 	public final static Point BOULDER_TILE_IN_SPRITE = new Point(3, 1);
+	
+	// Columns and rows
+	public final static int DEFAULT_TILES_BMP_COLUMNS = 6;
+	public final static int DEFAULT_TILES_BMP_ROWS = 3;
+	public static final int DEFAULT_PLAYER_BMP_ROWS = 4;
+	public static final int DEFAULT_PLAYER_BMP_COLUMNS = 3;
 }
--- IceCaveProject/src/com/android/icecave/gui/GameTheme.java
+++ IceCaveProject/src/com/android/icecave/gui/GameTheme.java
@@ -1,11 +1,14 @@
 package com.android.icecave.gui;
 
 import android.graphics.Bitmap;
+import com.android.icecave.general.Consts;
+import com.android.icecave.general.EDirection;
 
 /**
  * Class to represent the over all game theme.
+ * 
  * @author Tom
- *
+ * 
  */
 public class GameTheme
 {
@@ -14,28 +17,67 @@ public class GameTheme
 
 	/**
 	 * Create a new instance of the GameTheme object.
-	 * @param tilesTheme - The current tiles theme.
-	 * @param playerTheme - The current player theme.
+	 * 
+	 * @param tilesTheme
+	 *            - The current tiles theme.
+	 * @param playerTheme
+	 *            - The current player theme.
 	 */
-	public GameTheme(Bitmap tilesTheme, Bitmap playerTheme) {
+	public GameTheme(Bitmap tilesTheme, Bitmap playerTheme)
+	{
 		mTilesTheme = tilesTheme;
 		mPlayerTheme = playerTheme;
 	}
-	
+
 	/**
 	 * Get the tiles theme of the game.
+	 * 
 	 * @return
 	 */
-	public Bitmap getTilesTheme() {
+	public Bitmap getTilesTheme()
+	{
 		return mTilesTheme;
 	}
-	
+
 	/**
 	 * Get the player theme of the game.
+	 * 
 	 * @return
 	 */
-	public Bitmap getPlayerTheme() {
+	public Bitmap getPlayerTheme()
+	{
 		return mPlayerTheme;
 	}
 
+	/***
+	 * 
+	 * @param direction
+	 *            Direction of where the player is moving towards
+	 * @return Index of the row of the tiles with the given direction
+	 */
+	public int getPlayerDirectionRow(EDirection direction)
+	{		
+		int result;
+		
+		switch (EDirection.values()[direction.ordinal()])
+		{
+		case DOWN:
+			result = Consts.PLAYER_DOWN_ROW;
+			break;
+		case LEFT:
+			result = Consts.PLAYER_LEFT_ROW;
+			break;
+		case RIGHT:
+			result = Consts.PLAYER_RIGHT_ROW;
+			break;
+		case UP:
+			result = Consts.PLAYER_UP_ROW;
+			break;
+		default:
+			result = Consts.PLAYER_ERROR_ROW;
+			break;
+		}
+		
+		return result;
+	}
 }
