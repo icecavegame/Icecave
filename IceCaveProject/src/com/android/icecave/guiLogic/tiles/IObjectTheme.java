0579f07aaca2a1df66316e5ff55edd1d0124f379
Sagie Levy <sagielevy21@gmail.com>
2013-10-04 16:20:24 +0300

    New feature, random tiles of same group type

---
+++ IceCaveProject/src/com/android/icecave/guiLogic/tiles/IObjectTheme.java
@@ -0,0 +1,8 @@
+package com.android.icecave.guiLogic.tiles;
+
+import com.android.icecave.utils.Point;
+
+public interface IObjectTheme
+{
+	public Point getRandomTilePosition();
+}
---
+++ IceCaveProject/src/com/android/icecave/guiLogic/tiles/ThemeMap.java
@@ -0,0 +1,26 @@
+package com.android.icecave.guiLogic.tiles;
+
+public class ThemeMap
+{
+	EmptyTheme mEmpty;
+	BoulderTheme mBoulder;
+	FlagTheme mFlag;
+	WallTheme mWall;
+	
+	public EmptyTheme getEmptyTheme()
+	{
+		return mEmpty;
+	}
+	public BoulderTheme getBoulderTheme()
+	{
+		return mBoulder;
+	}
+	public FlagTheme getFlagTheme()
+	{
+		return mFlag;
+	}
+	public WallTheme getWallTheme()
+	{
+		return mWall;
+	}
+}
