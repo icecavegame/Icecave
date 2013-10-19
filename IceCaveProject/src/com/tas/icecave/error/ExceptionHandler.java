e06b17b0947741d4f6164b96921329dacc2a48ae
Sagie Levy <sagielevy21@gmail.com>
2013-10-19 16:40:08 +0300

    Refresh

--- IceCaveProject/src/com/tas/icecave/error/ExceptionHandler.java
+++ IceCaveProject/src/com/tas/icecave/error/ExceptionHandler.java
@@ -8,18 +8,24 @@ import java.lang.Thread.UncaughtExceptionHandler;
 public class ExceptionHandler implements UncaughtExceptionHandler
 {
 	Object mToHandle;
-	
-	public ExceptionHandler (Object toHandle) {
+
+	public ExceptionHandler(Object toHandle)
+	{
 		mToHandle = toHandle;
 	}
 
 	@Override
 	public void uncaughtException(Thread arg0, Throwable arg1)
 	{
-		Log.e("Ice Cave", arg1.getMessage());
-		
-		Toast.makeText((Activity)mToHandle, "Sorry, it seems the game has crashed :(", Toast.LENGTH_SHORT).show();
+		if (arg1.getMessage() != null)
+		{
+			Log.e("Ice Cave", arg1.getMessage());
+		}
+
+		// Toast.makeText((Activity)mToHandle, "Sorry, it seems the game has crashed :(", Toast.LENGTH_SHORT).show();
+		((Activity) mToHandle).finish();
 		// TODO What else do we want to do here?
+
 	}
 
 }
